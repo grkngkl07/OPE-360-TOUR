9fef2db62f1a3fbc6b10cbc300fce814af98b112
grkngkl07 <grkngkl18@gmail.com>
2025-11-05 20:21:58 +0300

    initial upload

---
+++ data.js
@@ -0,0 +1,131 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-ope_360_01",
+      "name": "OPE_360_01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4089217169644996,
+          "pitch": 0.014958308094703554,
+          "rotation": 0,
+          "target": "2-ope_360_03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-ope_360_02",
+      "name": "OPE_360_02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.83289293530021,
+          "pitch": 0.021552717235108432,
+          "rotation": 0,
+          "target": "2-ope_360_03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-ope_360_03",
+      "name": "OPE_360_03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4982230240995449,
+          "pitch": 0.19331678904616467,
+          "rotation": 0,
+          "target": "0-ope_360_01"
+        },
+        {
+          "yaw": 1.0635929598815643,
+          "pitch": 0.05042238568158908,
+          "rotation": 0,
+          "target": "1-ope_360_02"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "OPE",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
